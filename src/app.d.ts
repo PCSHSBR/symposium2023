0cb1aef6bc24562f64270d786adb7d0fcc345e28
PhubestSrikooon <forbf100@gmail.com>
2023-06-08 19:17:32 +0700

    add : login form

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,20 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
+// src/app.d.ts
+
+import { SupabaseClient, Session } from '@supabase/supabase-js'
+import { Database } from './DatabaseDefinitions'
+
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+  namespace App {
+    interface Locals {
+      supabase: SupabaseClient<Database>
+      getSession(): Promise<Session | null>
+    }
+    interface PageData {
+      session: Session | null
+    }
+    // interface Error {}
+    // interface Platform {}
+  }
 }
 
 export {};
