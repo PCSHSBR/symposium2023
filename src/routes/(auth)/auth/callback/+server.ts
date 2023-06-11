280f004f8a8b749496f3c9aa6a44b601fb28e82f
Patsagorn <patsagorn_y@outlook.com>
2023-06-11 09:09:33 +0700

    feat: add next params

--- src/routes/(auth)/auth/callback/+server.ts
+++ src/routes/(auth)/auth/callback/+server.ts
@@ -3,10 +3,11 @@ import type { RequestHandler } from './$types';
 
 export const GET = (async ({ url, locals: { supabase } }) => {
 	const code = url.searchParams.get('code');
+	const next = url.searchParams.get('next')
 	console.log('code', code);
 	if (code) {
 		await supabase.auth.exchangeCodeForSession(code);
 	}
 
-	throw redirect(303, '/');
+	throw redirect(303, next || '/');
 }) satisfies RequestHandler;
