e48fb86eafadc6a221a1c264d3ac6e56c9cc150e
PhubestSrikooon <forbf100@gmail.com>
2023-06-16 21:24:13 +0700

    add : auth redirect

--- src/routes/(auth)/auth/+page.svelte
+++ src/routes/(auth)/auth/+page.svelte
@@ -1,35 +1,8 @@
-<script lang="ts">
-	import { goto } from '$app/navigation';
-
-	export let data;
-	let { supabase } = data;
-
-	let isLogginout = false;
-
-	const handleSignOut = async () => {
-		isLogginout = true;
-		let result = await supabase.auth.signOut();
-		if (result.error) {
-			alert(result.error.message);
-		}
-		isLogginout = false;
-		goto('/login');
-	};
-</script>
-
-<div class="flex h-screen items-center justify-center">
-	<div class="flex flex-col flex-wrap">
-		<p class="max-w-md overflow-y-auto">
-			{JSON.stringify(data.session) ?? 'คุณยังไม่เข้าสู่ระบบ โปรดเข้าสู่ระบบ'}
-		</p>
-		<button class="btn-error btn" on:click={handleSignOut} disabled={isLogginout}>
-			{#if isLogginout && data.session}
-				<span class="loading loading-spinner" />
-			{:else if !data.session}
-				เข้าสู่ระบบ
-			{:else}
-				ออกจากระบบ
-			{/if}</button
-		>
-	</div>
+<div class="flex justify-center items-center align-middle h-screen flex-col">
+  <span class="loading loading-ring loading-lg"></span>
+  <div>
+    <h1 class="text-4xl">Redirecting</h1>
+    <p>คุณจะ Redirect เร็วๆนี้</p>
+    <p>ถ้าไม่ได้ Redirect ให้กด <a href="/">ที่นี่</a></p>
+  </div>
 </div>
