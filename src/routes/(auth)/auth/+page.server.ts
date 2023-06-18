1c5c57db22fe6af887087af5fdbe0633944e3807
PhubestSrikooon <forbf100@gmail.com>
2023-06-18 19:57:51 +0700

    add : idk

---
+++ src/routes/(auth)/auth/+page.server.ts
@@ -0,0 +1,7 @@
+import { error, redirect } from "@sveltejs/kit"
+
+export const load = async ({ params, locals: { getSession } }) => {
+  const session = await getSession()
+  if (!session) throw redirect(302,"/login")
+  throw redirect(302,"controls/dashboard")
+}
---
+++ src/routes/(auth)/controls/dashboard/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import type { SupabaseClient } from '@supabase/supabase-js';
+	
+	export let data;
+	let { supabase } = data;
+
+	let isLogginout = false;
+
+	const handleSignOut = async () => {
+		isLogginout = true;
+		let result = await supabase.auth.signOut();
+		if (result.error) {
+			alert(result.error.message);
+		}
+		isLogginout = false;
+		goto('/login');
+	};
+</script>
+
+<div class="flex h-screen items-center justify-center">
+	<div class="flex flex-col flex-wrap">
+		<p class="max-w-md overflow-y-auto">
+			{JSON.stringify(data.session) ?? 'คุณยังไม่เข้าสู่ระบบ โปรดเข้าสู่ระบบ'}
+		</p>
+		<button class="btn-error btn" on:click={handleSignOut} disabled={isLogginout}>
+			{#if isLogginout && data.session}
+				<span class="loading loading-spinner" />
+			{:else if !data.session}
+				เข้าสู่ระบบ
+			{:else}
+				ออกจากระบบ
+			{/if}</button
+		>
+	</div>
+</div>
