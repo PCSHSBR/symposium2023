a8aca9ff50869828643c01390f5bc2c36761990e
Patsagorn <patsagorn_y@outlook.com>
2023-06-26 09:24:27 +0700

    ลืมใส่

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv-vault-core'
+
+dotenv.config();
 
 export default defineConfig({
 	plugins: [sveltekit()]
