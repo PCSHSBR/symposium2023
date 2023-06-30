3ba1569f361570e5c0e35f9f45995ee4eeef7ecd
Patsagorn <patsagorn_y@outlook.com>
2023-06-30 20:49:03 +0700

    update

--- src/routes/(auth)/welcome/+page.server.ts
+++ src/routes/(auth)/welcome/+page.server.ts
@@ -1,7 +1,17 @@
 import { error } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async ({ locals: { role, getSession } }) => {
 	const session = await getSession();
 	if (!session) throw error(401, 'Unauthorized');
 }) satisfies PageServerLoad;
+
+export const actions = {
+	default: async ({ locals: { getSession, supabase }, request }) => {
+		// check session
+		const session = await getSession();
+		if (!session) throw error(401, 'Unauthorized');
+		const formData = Object.fromEntries(await request.formData());
+		const { error, } = await supabase.auth.updateUser
+	}
+} satisfies Actions;
