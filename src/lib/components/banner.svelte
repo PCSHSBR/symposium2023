75a93443834be917645830376856ac6bc0d28106
PhubestSrikooon <forbf100@gmail.com>
2023-07-01 11:31:48 +0700

    fix camera abit

--- src/lib/components/banner.svelte
+++ src/lib/components/banner.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  export let isLoading = false;
+	export let isLoading = false;
 
 	import { onMount } from 'svelte';
 	import * as THREE from 'three';
@@ -19,14 +19,14 @@
 		let z = Math.sin((maxY - clientY * 2) / maxY) * intensity;
 		return new THREE.Vector3(x, 0, z);
 	}
-  export let isHover : boolean = false;
+	export let isHover: boolean = false;
 
 	onMount(() => {
 		let width = window.innerWidth;
 		let height = window.innerHeight;
 		let pointer = new THREE.Vector2();
-    let p1 = new THREE.Vector2();
-    let raycaster = new THREE.Raycaster();
+		let p1 = new THREE.Vector2();
+		let raycaster = new THREE.Raycaster();
 		let rotation = new THREE.Vector3();
 		let camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 5000);
 		let scene = new THREE.Scene();
@@ -38,41 +38,44 @@
 		renderer.shadowMap.enabled = true;
 		renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 		threeCanvas?.appendChild(renderer.domElement);
-    
+
 		camera.position.x = 290;
 		camera.position.y = -400;
 		camera.position.z = 1400;
 		camera.rotation.y = 0.540353936417444;
 
-    isLoading = true;
-    //scene
+		isLoading = true;
+		//scene
 		const loader = new SplineLoader();
-		loader.load('https://prod.spline.design/eT-odXHjbLjvN6uT/scene.splinecode', (splineScene) => {
-			scene.add(splineScene);
-		},(event)=>{
-      console.log(event);
-    });
-    
-    let pointLight = new THREE.PointLight(0xffffff, 1, 5000);
-    let pointLightHelper = new THREE.PointLightHelper(pointLight, 100);
-    pointLight.position.set(-200, -200, 0);
-    pointLight.castShadow = true;
-    pointLight.shadow.mapSize.width = 2048;
-    pointLight.shadow.mapSize.height = 2048
-    
-    scene.add(pointLight, 
-    // pointLightHelper
-    );
-    
-    
-    
+		loader.load(
+			'https://prod.spline.design/eT-odXHjbLjvN6uT/scene.splinecode',
+			(splineScene) => {
+				scene.add(splineScene);
+			},
+			(event) => {
+				console.log(event);
+			}
+		);
+
+		let pointLight = new THREE.PointLight(0xffffff, 1, 5000);
+		let pointLightHelper = new THREE.PointLightHelper(pointLight, 100);
+		pointLight.position.set(-200, -200, 0);
+		pointLight.castShadow = true;
+		pointLight.shadow.mapSize.width = 2048;
+		pointLight.shadow.mapSize.height = 2048;
+
+		scene.add(
+			pointLight
+			// pointLightHelper
+		);
+
 		// composer
-    let composer = new EffectComposer(renderer);
+		let composer = new EffectComposer(renderer);
 		composer.addPass(new RenderPass(scene, camera));
 		const ssaoPass = new SSAOPass(scene, camera, width, height);
 		ssaoPass.kernelRadius = 16;
 		composer.addPass(ssaoPass);
-    composer.addPass(new UnrealBloomPass(new THREE.Vector2(width, height), 1.5, 0.4, 0.0));
+		composer.addPass(new UnrealBloomPass(new THREE.Vector2(width, height), 1.5, 0.4, 0.0));
 		let outputPass = new ShaderPass(GammaCorrectionShader);
 		composer.addPass(outputPass);
 
@@ -80,12 +83,18 @@
 			pointer = new THREE.Vector2(p.clientX, p.clientY);
 			rotation = calculate3DRotation(width, height, p.x, p.y, 0.1);
 
-      p1.x = ( p.clientX / window.innerWidth ) * 2 - 1;
-			p1.y = - ( p.clientY / window.innerHeight ) * 2 + 1;
+			p1.x = (p.clientX / window.innerWidth) * 2 - 1;
+			p1.y = -(p.clientY / window.innerHeight) * 2 + 1;
 
-			raycaster.setFromCamera( p1, camera );
-			const intersects = raycaster.intersectObjects( scene.children, true );
-      pointLight.position.set(intersects[0].point.x + 100,intersects[0].point.y +100,intersects[0].point.z +100);
+			raycaster.setFromCamera(p1, camera);
+			const intersects = raycaster.intersectObjects(scene.children, true);
+			if (isHover) {
+				pointLight.position.set(
+					intersects[0].point.x + 100,
+					intersects[0].point.y + 100,
+					intersects[0].point.z + 100
+				);
+			}
 		});
 		window.addEventListener('resize', () => {
 			width = window.innerWidth;
@@ -96,23 +105,17 @@
 			composer.setSize(width, height);
 		});
 
-
 		function animation() {
 			requestAnimationFrame(animation);
 			renderer.render(scene, camera);
-			// controls.update();
-			if (isHover){
-        camera.rotation.set(rotation.z, 0.540353936417444 + rotation.x, 0);
-      }
-      else{
-        camera.rotation.set(
-          Math.sin(new Date().getTime() * 0.0004) * 0.1 ,
-           0.540353936417444 + Math.sin(new Date().getTime() * 0.0005) * 0.1,
-           Math.sin(new Date().getTime() * 0.0003) * 0.1);
-      }
+			// camera.rotation.set(
+			// 	Math.sin(new Date().getTime() * 0.0004) * 0.1,
+			// 	0.540353936417444 + Math.sin(new Date().getTime() * 0.0005) * 0.1,
+			// 	Math.sin(new Date().getTime() * 0.0003) * 0.1
+			// );
 		}
 		animation();
 	});
 </script>
 
-<div class="three-canvas"/>
+<div class="three-canvas" />
--- src/routes/(contents)/+page.svelte
+++ src/routes/(contents)/+page.svelte
@@ -1,30 +1,21 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import Banner from '$lib/components/banner.svelte';
+	import Icon from '@iconify/svelte';
 
-	let days = 15;
-	let hours = 15;
-	let minutes = 12;
-	let seconds = 24;
 	let isLoading = true;
 	let isBannerHovered = false;
+	var isSafari: boolean;
+	let eventdate = new Date('2023-09-20');
+	let now = new Date();
+	let remaining = new Date(eventdate.getTime() - now.getTime());
 
+	console.log(now.toISOString());
 	setInterval(() => {
-		seconds--;
-		if (seconds == 0) {
-			minutes--;
-			seconds = 59;
-		}
-		if (minutes == 0) {
-			hours--;
-			minutes = 59;
-		}
-		if (hours == 0) {
-			days--;
-			hours = 23;
-		}
+		now = new Date();
+		remaining = new Date(eventdate.getTime() - now.getTime());
 	}, 1000);
-	var isSafari : boolean;
+
 	onMount(() => {
 		isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 		console.log(isSafari);
@@ -44,19 +35,33 @@
 	<meta property="og:title" content="The 3rd PCSHS Science Symposium 2023 @PCSHSBR" />
 </svelte:head>
 
-<section class="banner relative" on:mouseenter={()=>{isBannerHovered=true}} on:mouseleave={()=>{isBannerHovered=false}}>
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<section
+	class="banner relative" 
+	on:mouseenter={() => {
+		isBannerHovered = true;
+	}}
+	on:mouseleave={() => {
+		isBannerHovered = false;
+	}}
+>
 	<div class="h-screen overflow-hidden">
-		<Banner bind:isLoading={isLoading} bind:isHover={isBannerHovered}/>
+		<Banner bind:isLoading bind:isHover={isBannerHovered} />
 	</div>
 	<div class="absolute top-[15%] px-16 md:px-32">
 		<h1
 			class="-leading-[16px] text-4xl font-black mix-blend-difference invert dark:mix-blend-difference dark:invert-0 md:text-6xl"
 		>
-			<span class="text-[128px]">3</span><span class="text-xl align-top">rd</span> PCSHS Science Symposium
+			<span class="text-[128px]">3</span><span class="align-top text-xl">rd</span> PCSHS Science Symposium
 		</h1>
 		<p class="mix-blend-difference invert dark:mix-blend-difference dark:invert-0">
 			มาร่วมสร้างสรรค์นวัตกรรมที่จะเปลี่ยนโลกให้ดีขึ้นผ่านการนำเสนอผลงานโครงงานของคุณกันเถอะ!
 		</p>
+		<!-- svelte-ignore a11y-missing-attribute -->
 		<a class="btn-primary btn">ส่งโครงงาน</a>
+	</div>
+	<div class="absolute bottom-0 flex h-16 w-full flex-col items-center">
+		<Icon icon="mdi:menu-down" />
+		<span>ดูเพิมเติม</span>
 	</div>
 </section>
