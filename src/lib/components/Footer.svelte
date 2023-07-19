0e6a54fc4c414f67ce6ff0ac8e2ca70e86b2d5e5
PhubestSrikooon <forbf100@gmail.com>
2023-07-19 18:38:01 +0700

    update : footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
+	import LogoWithText from './LogoWithText.svelte';
 	import Icon from '@iconify/svelte';
 </script>
 
-<footer class="footer footer-center rounded bg-base-200 p-10 text-base-content">
-	<div class="flex w-full justify-between">
-		<div>
+<footer class="footer footer-center rounded bg-base-200 p-16 text-base-content">
+	<div class="flex w-full flex-wrap justify-between">
+		<div class="text-left">
+			<LogoWithText class="h-20 w-auto" />
 			<p>Copyright © 2023 PCSHSBR Science Symposium</p>
 		</div>
-		<div class="flex flex-row">
-			<a href="/facebook"><Icon icon="ic:baseline-facebook" class="text-2xl" /></a>
-			<a href="/facebook"><Icon icon="mdi:youtube" class="text-2xl" /></a>
-			<a href="/facebook"><Icon icon="mdi:email-outline" class="text-2xl" /></a>
+		<div class="flex flex-wrap gap-x-10">
+			<div class="text-left">
+				<h3 class="text-xl font-bold">ติดต่อ</h3>
+				<div class="flex">
+					<a href="/facebook" class="flex"><Icon icon="ic:baseline-facebook" class="text-2xl" /></a>
+					<a href="/facebook" class="flex"><Icon icon="mdi:phone" class="text-2xl" /></a>
+					<a href="/facebook" class="flex"><Icon icon="mdi:email-outline" class="text-2xl" /></a>
+				</div>
+			</div>
 		</div>
 	</div>
 </footer>
