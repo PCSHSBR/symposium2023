98a6b643fc1404f7c19749bb7a7587a5e1a84e2b
PhubestSrikoon <forbf100@gmail.com>
2023-08-21 17:59:58 +0700

    fix : z-index

--- src/lib/components/ShirtShow.svelte
+++ src/lib/components/ShirtShow.svelte
@@ -8,7 +8,7 @@
 			class="main-c-1 relative mx-auto h-[30rem] max-w-[60rem] overflow-hidden rounded-3xl bg-gradient-to-tl from-primary to-secondary transition-all duration-500"
 		>
 			<div>
-				<div class="text-c-1 absolute right-0 z-[1000] text-white transition-all duration-500">
+				<div class="text-c-1 absolute right-0 z-[100] text-white transition-all duration-500">
 					<div class="text-c-2 max-w-[400px] p-12 text-right transition-all duration-500">
 						<h3 class="w-full max-w-[400px] text-4xl font-bold sm:text-6xl">
 							ซื้อเสื้อ Symposium ได้เลยตอนนี้
@@ -17,7 +17,7 @@
 						<!-- <button class="btn w-full bg-white text-black"></button> -->
 					</div>
 				</div>
-				<div class="left-0 z-[999]">
+				<div class="left-0 z-[99]">
 					<CdnImage
 						class="absolute bottom-0 left-10 h-[360px] !bg-none md:left-[120px] md:h-full"
 						file="sym2023/cut-images/IMG-0027-1.png"
