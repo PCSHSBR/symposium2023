69eded914f189db2c0245ef67615ad276daa9195
PhubestSrikoon <forbf100@gmail.com>
2023-09-02 16:17:04 +0700

    add : maptiler

--- src/routes/(contents)/map/+page.svelte
+++ src/routes/(contents)/map/+page.svelte
@@ -1,145 +1,33 @@
 <script lang="ts">
-	import { onMount, onDestroy } from 'svelte';
-	import * as THREE from 'three';
-	import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-	import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-	import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 	import { browser } from '$app/environment';
 	import Icon from '@iconify/svelte';
-
-	const clearEventListenerController = new AbortController();
-	let animationFrameID: number;
-
-	// credit for ongsalt for optimization
-
-	let threeCanvas: HTMLDivElement;
-
+	import maplibre from 'maplibre-gl';
+	import { onDestroy, onMount } from 'svelte';
+	let map: HTMLDivElement;
 	onMount(() => {
-		async function threejs() {
-			let height = window.innerHeight;
-			let width = window.innerWidth;
-			const pointer = new THREE.Vector2();
-			const loader = new GLTFLoader();
-			const camera = new THREE.PerspectiveCamera(
-				50,
-				window.innerWidth / window.innerHeight,
-				0.1,
-				1000
-			);
-			const scene = new THREE.Scene();
-			const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-			const raycaster = new THREE.Raycaster();
-			const controls = new OrbitControls(camera, renderer.domElement);
-			let composer = new EffectComposer(renderer);
-
-			// controls.enablePan = false;
-			controls.maxPolarAngle = (Math.PI - 0.1) / 2; // Limit angle of visibility
-			controls.minZoom = 1;
-
-			renderer.setSize(window.innerWidth, window.innerHeight);
-			renderer.shadowMap.enabled = true;
-			renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-			renderer.autoClear = true;
-			threeCanvas?.appendChild(renderer.domElement);
-
-			camera.position.x = 0;
-			camera.position.y = 7;
-			camera.rotation.x = -(Math.PI / 2);
-
-			// scene objwcts
-			let ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-			let directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-			let SchoolModel = await loadGLTFModel('models/School Model.glb', true, scene);
-			SchoolModel.scene.rotation.y = Math.PI / 2;
-			setShadow(SchoolModel.scene, true, true, ['Base', 'Sport3', 'Forest1']);
-
-			directionalLight.position.set(5, 10, 0);
-			directionalLight.shadow.radius = 0.5;
-			directionalLight.rotation.z = -Math.PI / 4;
-			// directionalLight.castShadow = true;
-
-			// add to scene
-			scene.add(directionalLight, ambientLight);
-
-			// composer
-
-			//end
-			function animation() {
-				animationFrameID = requestAnimationFrame(animation);
-				renderer.render(scene, camera);
-				composer.render();
-				controls.update();
-				composer.setSize(width, height);
-				console.log(camera.position, camera.rotation);
-			}
-
-			{
-				window.addEventListener('resize', (event) => {
-					height = window.innerHeight;
-					width = window.innerWidth;
-					camera.aspect = width / height;
-					camera.updateProjectionMatrix();
-					renderer.setSize(width, height);
-				});
-			}
-			animation();
-		}
-		interface GLTFResult {
-			scene: THREE.Group;
-		}
-		function loadGLTFModel(
-			url: string,
-			autoadd?: boolean | undefined,
-			pscene?: THREE.Scene | undefined
-		): Promise<GLTFResult> {
-			return new Promise((resolve, reject) => {
-				const loader = new GLTFLoader();
-
-				loader.load(
-					url,
-					(gltf) => {
-						const result: GLTFResult = {
-							scene: gltf.scene
-						};
-						autoadd ? (pscene ? pscene.add(gltf.scene) : null) : null;
-						resolve(result);
-					},
-					undefined,
-					(error) => {
-						reject(error);
-					}
-				);
-			});
-		}
-		function setShadow(
-			scene: THREE.Group,
-			castShadow?: boolean | false,
-			receiveShadow?: boolean | false,
-			exceptcast = [] as string[]
-		) {
-			scene.traverse((node) => {
-				if ((node as any).isMesh) {
-					(node as any).receiveShadow = receiveShadow;
-					if (exceptcast.includes((node as any).name)) {
-						return;
-					}
-					(node as any).castShadow = castShadow;
-				}
-			});
-		}
-
-		threejs();
+		const map1 = new maplibre.Map({
+			container: map,
+			style:
+				'https://api.maptiler.com/maps/5aa5e922-4fbe-4207-bb97-01a9e837c6ab/style.json?key=tX3B0e0L23CmIJmoim13',
+			center: [103.3041, 15.30784],
+			zoom: 18,
+			maxBounds: [
+				[103.313, 15.304],
+				[103.309, 15.31]
+			]
+		});
+		map1.addControl(new maplibre.NavigationControl());
+		//tilted the map to make it look 3D
+		map1.setPitch(60);
 	});
-
 	onDestroy(() => {
-		if (browser) {
-			clearEventListenerController.abort();
-			cancelAnimationFrame(animationFrameID);
-		}
+		if (!browser) return;
+		map.remove();
 	});
 </script>
 
 <div class="relative">
+	<div class="map h-[calc(100vh-5rem)]" id="map" bind:this={map} />
 	<div class="absolute left-4 top-4">
 		<div class="flex gap-1">
 			<a href="/#school-map">
@@ -149,7 +37,7 @@
 				</button>
 			</a>
 			<a href="https://goo.gl/maps/hrtCinUXAnTnRN1FA">
-				<button class="btn-primary btn"
+				<button class="btn btn-primary"
 					><Icon icon="mdi:google-maps" class="h-6 w-6 sm:h-4 sm:w-4" /><span
 						class="hidden sm:inline"
 					>
@@ -165,5 +53,5 @@
 			</p>
 		</div>
 	</div>
-	<div class="three-canvas h-[100vh-4rem] w-screen" bind:this={threeCanvas} />
 </div>
+<link href="https://cdn.maptiler.com/maptiler-sdk-js/v1.1.1/maptiler-sdk.css" rel="stylesheet" />
