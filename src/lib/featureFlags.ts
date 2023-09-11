e6b03e17b2ae4b69a41ebd74eec386dd4822adc2
Patsagorn <patsagorn_y@outlook.com>
2023-09-12 04:23:01 +0700

    ff: disabled login

--- src/lib/featureFlags.ts
+++ src/lib/featureFlags.ts
@@ -2,14 +2,15 @@
  * This is setting file for feature flags.
  * Include all switchs for features and some other settings for website.
  *
+ * ! THIS FILE IS PUBLIC.
+ *
  * ! CAUTION: TAKE CARE OF WHAT YOU ARE DOING.
  * ! DO NOT CHANGE ANYTHING IF YOU ARE NOT SURE.
  * ! DO NOT ACCEPT ANY PULL REQUESTS ABOUT THIS FILE.
  */
 
 export const featureFlags = {
-	// Switch for feature
-	openForLogin: true,
+	openForLogin: false,
 	disableInviteNewUser: true,
 	openForRegistrationAndEditProject: false,
 	openForUploadAbstract: false,
